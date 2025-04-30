e94e5f4d0c52ed9e54032ce34d0645bdce9eb7cd
Venky3153 <venkysmart315@gmail.com>
2025-04-30 15:27:21 +0530

    Add files via upload

---
+++ ReturnMethod.java
@@ -0,0 +1,37 @@
+public class ReturnMethod {
+	
+		
+		
+	
+	
+public static int sum(int a,int c) {
+		return a +c;
+	}
+	
+	public static int multplication(int b, int d) {
+		
+		int z = b+d;
+		return z;
+		
+		
+		
+	}
+	
+	
+	
+	public static void main(String [] args) {
+	   int result = sum(23,43);
+	System.out.println("result:"+result);
+		
+	int demo =sum(2,3);	
+	System.out.println("demo:"+demo);
+		
+	}
+		
+	}	
+		
+		
+	
+	
+
+
